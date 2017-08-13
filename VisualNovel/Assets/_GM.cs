d9043670ae7b0147873699e17f04936b15bc66f8
Jack Tugby <masterjackt@hotmail.co.uk>
2017-08-14 00:57:33 +0100

    VisualNovel Project Upload

---
+++ VisualNovel/Assets/_GM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class _GM : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
